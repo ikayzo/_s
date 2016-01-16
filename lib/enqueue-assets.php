08a42a506a30251bad3762271ffcb4fc63d4a073
Nainoa Shizuru <nainoa@ikayzo.com>
2016-01-15 16:36:53 -1000

    Update enqueue-assets.php

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -36,7 +36,6 @@ function _s_scripts() {
 	// Upgrade Wordpress copy of jQuery
 	/*=================================
     function jquery_enqueue() {
-        wp_deregister_script('jquery');
         wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", false, null);
         wp_enqueue_script('jquery');
     }
