49f05cad1e4947d395c2214aa7e795de80fc4391
Nainoa Shizuru <nainoa@ikayzo.com>
2016-09-29 16:23:10 -1000

    Removed custom excerpt length function

--- lib/extras.php
+++ lib/extras.php
@@ -14,12 +14,12 @@
  * @return array
  */
 function _s_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+  // Adds a class of group-blog to blogs with more than 1 published author.
+  if ( is_multi_author() ) {
+    $classes[] = 'group-blog';
+  }
 
-	return $classes;
+  return $classes;
 }
 add_filter( 'body_class', '_s_body_classes' );
 
@@ -28,8 +28,8 @@ add_filter( 'body_class', '_s_body_classes' );
  * Adds SVG file support when uploading to media library
  */
 function cc_mime_types( $mimes ){
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
 
@@ -47,36 +47,21 @@ add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return n
 add_filter('template_include', 'custom_template_include');
 
 function custom_template_include($template) {
-	$custom_template_location = get_stylesheet_directory() . '/templates/single/';
+  $custom_template_location = get_stylesheet_directory() . '/templates/single/';
 
-	 if ( get_post_type () ) {
+   if ( get_post_type () ) {
 
-		if ( is_archive() ) :
-		   if(file_exists($custom_template_location . 'archive-' . get_post_type() . '.php'))
-			  return $custom_template_location . 'archive-' . get_post_type() . '.php';
-		endif;
+    if ( is_archive() ) :
+       if(file_exists($custom_template_location . 'archive-' . get_post_type() . '.php'))
+        return $custom_template_location . 'archive-' . get_post_type() . '.php';
+    endif;
 
-		if ( is_single() ) :
-		   if(file_exists($custom_template_location . 'single-' . get_post_type() . '.php'))
-			  return $custom_template_location . 'single-' . get_post_type() . '.php';
-		endif;
+    if ( is_single() ) :
+       if(file_exists($custom_template_location . 'single-' . get_post_type() . '.php'))
+        return $custom_template_location . 'single-' . get_post_type() . '.php';
+    endif;
 
-	}
+  }
 
-	return $template;
+  return $template;
 }
-
-
-/**
- * Customize the post excerpt
- */
-function custom_excerpt_length( $length ) {
-	return 30;
-}
-
-function new_excerpt_more( $more ) {
-	return '...';
-}
-
-add_filter('excerpt_more', 'new_excerpt_more');
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
