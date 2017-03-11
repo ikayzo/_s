0eddf7a15c7eba0bf67168b9ee93b15a5bfd9feb
Nainoa Shizuru <nainoa@ikayzo.com>
2017-03-10 16:57:19 -1000

    Updated functions

--- lib/optimizations.php
+++ lib/optimizations.php
@@ -14,15 +14,15 @@ Admin
 * Remove unecessary dashboard widgets
 */
 function remove_dashboard_meta() {
-        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+  remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
 }
 
 add_action( 'admin_init', 'remove_dashboard_meta' );
@@ -35,16 +35,16 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 * (removes admin bar links)
 */
 function remove_admin_bar_links() {
-    global $wp_admin_bar;
-    $wp_admin_bar->remove_menu('wp-logo');          // Remove the WordPress logo
-    $wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
-    $wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
-    $wp_admin_bar->remove_menu('documentation');    // Remove the WordPress documentation link
-    $wp_admin_bar->remove_menu('support-forums');   // Remove the support forums link
-    $wp_admin_bar->remove_menu('feedback');         // Remove the feedback link
-    $wp_admin_bar->remove_menu('updates');          // Remove the updates link
-    $wp_admin_bar->remove_menu('comments');         // Remove the comments link
-    $wp_admin_bar->remove_menu('new-content');      // Remove the content link
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('wp-logo');          // Remove the WordPress logo
+  $wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
+  $wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
+  $wp_admin_bar->remove_menu('documentation');    // Remove the WordPress documentation link
+  $wp_admin_bar->remove_menu('support-forums');   // Remove the support forums link
+  $wp_admin_bar->remove_menu('feedback');         // Remove the feedback link
+  $wp_admin_bar->remove_menu('updates');          // Remove the updates link
+  $wp_admin_bar->remove_menu('comments');         // Remove the comments link
+  $wp_admin_bar->remove_menu('new-content');      // Remove the content link
 }
 
 add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
@@ -54,19 +54,19 @@ add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
  * Hide unecessary widgets (Appearance -> Widgets)
  */
 function unregister_default_wp_widgets() {
-    unregister_widget('WP_Widget_Pages');
-    unregister_widget('WP_Widget_Calendar');
-    unregister_widget('WP_Widget_Archives');
-    unregister_widget('WP_Widget_Links');
-    unregister_widget('WP_Widget_Meta');
-    unregister_widget('WP_Widget_Search');
-    // unregister_widget('WP_Widget_Text');
-    unregister_widget('WP_Widget_Categories');
-    unregister_widget('WP_Widget_Recent_Posts');
-    unregister_widget('WP_Widget_Recent_Comments');
-    unregister_widget('WP_Widget_RSS');
-    unregister_widget('WP_Widget_Tag_Cloud');
-    unregister_widget('WP_Nav_Menu_Widget');
+  unregister_widget('WP_Widget_Pages');
+  unregister_widget('WP_Widget_Calendar');
+  unregister_widget('WP_Widget_Archives');
+  unregister_widget('WP_Widget_Links');
+  unregister_widget('WP_Widget_Meta');
+  unregister_widget('WP_Widget_Search');
+  // unregister_widget('WP_Widget_Text');
+  unregister_widget('WP_Widget_Categories');
+  unregister_widget('WP_Widget_Recent_Posts');
+  unregister_widget('WP_Widget_Recent_Comments');
+  unregister_widget('WP_Widget_RSS');
+  unregister_widget('WP_Widget_Tag_Cloud');
+  unregister_widget('WP_Nav_Menu_Widget');
 }
 
 add_action('widgets_init', 'unregister_default_wp_widgets', 1);
@@ -77,7 +77,7 @@ add_action('widgets_init', 'unregister_default_wp_widgets', 1);
  * Hide meta boxes on page edit screen
  */
 function remove_page_meta_boxes() {
-    remove_meta_box('postcustom', 'page', 'normal');
+  remove_meta_box('postcustom', 'page', 'normal');
 }
 
 add_action( 'admin_menu' , 'remove_page_meta_boxes' );
@@ -88,7 +88,7 @@ add_action( 'admin_menu' , 'remove_page_meta_boxes' );
  * Hide meta boxes on post edit screen
  */
 function remove_post_meta_boxes() {
-    remove_meta_box( 'postcustom' , 'post' , 'normal' );
+  remove_meta_box( 'postcustom' , 'post' , 'normal' );
 }
 
 add_action( 'admin_menu' , 'remove_post_meta_boxes' );
@@ -99,24 +99,11 @@ add_action( 'admin_menu' , 'remove_post_meta_boxes' );
  * Disable help dropdown
  */
 function disable_help_dropdown($old_help, $screen_id, $screen){
-    $screen->remove_help_tabs();
-    return $old_help;
+  $screen->remove_help_tabs();
+  return $old_help;
 }
 
 add_filter( 'contextual_help', 'disable_help_dropdown', 999, 3 );
-
-
-
-/**
- * Remove 'Appearance' submenu items (optional)
- */
-function remove_appearance_menus () {
-    global $submenu;
-    unset($submenu['themes.php'][6]); // Customize
-    unset($submenu['themes.php'][20]); // Background
-}
-
-add_action('admin_menu', 'remove_appearance_menus');
 
 
 
@@ -154,10 +141,10 @@ Site
  */
 function _remove_query_strings( $src ){
   $rqs = explode( '?ver', $src );
-    return $rqs[0];
+  return $rqs[0];
 }
 
 if ( !is_admin() ) {
-    add_filter( 'script_loader_src', '_remove_query_strings', 15, 1 );
-    add_filter( 'style_loader_src', '_remove_query_strings', 15, 1 );
+  add_filter( 'script_loader_src', '_remove_query_strings', 15, 1 );
+  add_filter( 'style_loader_src', '_remove_query_strings', 15, 1 );
 }
